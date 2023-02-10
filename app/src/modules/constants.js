d601d8657b476894e6f7509514ba5501e45fce61
CandyDEV1014 <lancakster512@gmail.com>
2023-02-10 20:18:11 +0200

    initialize project

---
+++ app/src/modules/constants.js
@@ -0,0 +1,33 @@
+export const IPC_HANDLERS = {
+  AUTH: "auth",
+  DATABASE: "database",
+  FILE_SYSTEM: "fileSystem",
+};
+
+export const IPC_FUNCTIONS = {
+  SIGN_IN: "signIn",
+  INITIAL: "initial",
+  GET_PROFILE: "getProfile",
+  GET_SCOOTER_LIST: "getScooterList",
+  GET_SCOOTER_BY_ID: "getScooterById",
+  ADD_SCOOTER: "addScooter",
+  UPDATE_SCOOTER: "updateScooter",
+  DELETE_SCOOTER: "deleteScooter",
+  UPLOAD_SIGNATURE: "uploadSignature",
+  CHECK_SERVER_STATUS: "checkServerStatus",
+};
+
+export const STATUSES = {
+  SUCCESS: "success",
+  ERROR: "error",
+  WARNING: "warning",
+};
+
+export const ACTIONS = {
+  ADD: "add",
+  EDIT: "edit",
+  DELETE: "delete",
+};
+
+export const USER_ROLE = ["admin", "leader", "member"];
+export const SCOOTER_STATUS = ["IN LUCRU", "FINALIZAT", "IESIT"];
---
+++ frontend/src/views/dashboard/Dashboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <v-btn color="primary" dark class="mb-2 mr-2" to="/admin/scooter/add">
+      Add Scooter
+    </v-btn>
+    <v-row>
+      <v-col xs="12" sm="12" md="6">
+        <work-list />
+      </v-col>
+      <v-col xs="12" sm="12" md="6">
+        <completed-list />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import WorkList from "./WorkList.vue";
+import CompletedList from "./CompletedList.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    WorkList,
+    CompletedList,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.dialog-title {
+  font-size: 18px !important;
+  font-weight: 500;
+  line-height: 24px;
+}
+</style>
